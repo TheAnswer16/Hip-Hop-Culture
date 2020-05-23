fdbb9fc4424d60e866e53226d457e3901ca2d0bb
TheAnswer16 <64368305+TheAnswer16@users.noreply.github.com>
2020-05-23 15:42:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-let numero = 2;
-
-function mudarPlanoDeFundo(numero){
-	const fundo = document.querySelector('body');
-	fundo.style['background-image'] = `url(fundo-${numero}.jpg`;
-
-}
-function atualizarFundo(){
-	mudarPlanoDeFundo(numero);
-
-	if(numero === 3) numero = 1;
-	else numero++;
-
-
-}
-
-setInterval(atualizarFundo, 1000);
+let numero=2;function mudarPlanoDeFundo(numero){const fundo=document.querySelector('header');fundo.style['background-image']=`url(fundo-${numero}.jpg`;}
+function atualizarFundo(){mudarPlanoDeFundo(numero);if(numero===3)numero=1;else numero++;}
+setInterval(atualizarFundo,1000);
